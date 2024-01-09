c803aac09544a54b7ae318c7c1e4ecc34df58f42
aman baghel <amanbaarish@gmail.com>
2024-01-09 18:59:37 +0530

    origin

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DeleteView,CustomLoginView,Logout
+
+
+
+urlpatterns = [
+    path('login/',CustomLoginView.as_view(),name='login'),
+     path('logout/', Logout , name='logout'),
+    path('',TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('task-create/',TaskCreate.as_view(),name='task-create'),
+    path('task-update/<int:pk>/',TaskUpdate.as_view(),name='task-update'),
+    path('task-delete/<int:pk>/',DeleteView.as_view(),name='task-delete'),
+]
